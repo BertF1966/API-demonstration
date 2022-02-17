7712a6b86949f9bb68595850069692b147fc71c7
bertf1966 <bertf1966@gmail.com>
2022-02-17 22:10:17 +0000

    ticket 21, all tests passing

--- errors.js
+++ errors.js
@@ -1,5 +1,3 @@
-const { rows } = require("pg/lib/defaults");
-
 exports.handlePsqlErrors = (err, req, res, next) => {
   if (err.code === "22P02") {
     res.status(400).send({ msg: "bad request" });
