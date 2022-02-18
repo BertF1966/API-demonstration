417bbee7b928f0c9300390bd85e2b18a356533a7
bertf1966 <bertf1966@gmail.com>
2022-02-18 10:18:33 +0000

    removed console.log

--- errors.js
+++ errors.js
@@ -7,7 +7,6 @@ exports.handlePsqlErrors = (err, req, res, next) => {
 };
 
 exports.handleCustomErrors = (err, req, res, next) => {
-  console.log(err)
   if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
   }
