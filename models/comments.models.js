dd8bcfb6b1567967146ebb33f69d5ad99e2b65c5
bertf1966 <bertf1966@gmail.com>
2022-05-19 18:23:46 +0100

    add comments descending to most recent posted

--- models/comments.models.js
+++ models/comments.models.js
@@ -2,8 +2,13 @@ const db = require('../db/connection');
 
 exports.fetchComments = (articleId) => {
     return db
-    .query(`SELECT comments.* FROM comments LEFT JOIN articles ON comments.article_id = articles.article_id WHERE articles.article_id = $1 GROUP BY comments.comment_id;`, [articleId])
+    .query(`SELECT comments.* FROM comments 
+    LEFT JOIN articles ON comments.article_id = articles.article_id 
+    WHERE articles.article_id = $1 
+    GROUP BY comments.comment_id
+    ORDER BY comments.created_at DESC;`, [articleId])
     .then(({rows}) => {
+        console.log(rows)
         if (rows.length === 0) {
         return Promise.reject({ status: 404, msg: "Article not found" });
             
